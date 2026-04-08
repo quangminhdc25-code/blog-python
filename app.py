9d378c956489186d29c5051c67bc231eaf4c6482
quangminhdc25-code <quangminhdc25@gmail.com>
2026-04-08 15:19:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "<h1>Hello, đây là blog của tôi</h1>"
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
